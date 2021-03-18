b52ef6bdd94d79de51fd8c808604c4653d946e6a
Joseph Davis <joethebartenderpdx@gmail.com>
2021-03-18 14:26:28 -0700

    Final product V.1

--- app.js
+++ app.js
@@ -1,5 +1,52 @@
-// import functions and grab DOM elements
+//  Create variables
+const input1 = document.getElementById('input-1');
+const input2 = document.getElementById('input-2');
 
-// initialize state
+const addition = document.getElementById('addition');
+const subtraction = document.getElementById('subtraction');
+const multiplication = document.getElementById('multiplication');
+const division = document.getElementById('division');
 
-// set event listeners to update state and DOM
+const answer = document.getElementById('answer');
+
+const clear = document.getElementById('clear');
+
+// Event Listeners
+
+addition.addEventListener('click', () => {
+    const value1 = input1.value;
+    const value2 = input2.value;
+    answer.textContent = Number(value1) + Number(value2);
+    value1.textContent = '';
+    value2.textContent = '';
+});
+
+subtraction.addEventListener('click', () => {
+    const value1 = input1.value;
+    const value2 = input2.value;
+    answer.textContent = Number(value1) - Number(value2);
+    value1.textContent = "";
+    value2.textContent = "";
+});
+
+multiplication.addEventListener('click', () => {
+    const value1 = input1.value;
+    const value2 = input2.value;
+    answer.textContent = Number(value1) * Number(value2);
+    value1.value = "";
+    value2.value = "";
+});
+
+division.addEventListener('click', () => {
+    const value1 = input1.value;
+    const value2 = input2.value;
+    answer.textContent = Number(value1) + Number(value2);
+    value1.textContent = "";
+    value2.textContent = "";
+});
+
+clear.addEventListener('click', () => {
+    input1.value = "";
+    input2.value = "";
+    answer.textContent = "0";
+});
